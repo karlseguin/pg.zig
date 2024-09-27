karlseguin/pg.zig

commit adc728df1f19d4ef67d009b80f4cf53aae2bcbd1

Try to "peek" at query results
PostgreSQL will report some errors when preparing a statement, but other errors
only when executing it. In some cases, this probably makes sense, but in others
it's a bit unexpected.

For example:
select * from does_not_exist <-- fails to prepare

While:
create table already_exists <-- prepares, but fails to execute

We can try to present a more cohensive API by peeking at the first message after
a successful prepare. If it's an error, we can return that as part of the query!
call. If it isn't, we can leave it there for the first call result.next().

src/conn.zig
@@ -211,7 +211,11 @@ pub const Conn = struct {
         inline for (values) |value| {
             try stmt.bind(value);
         }
-        return stmt.execute();
+
+        return stmt.execute() catch |err| {
+            self.maybeRelease(opts.release_conn);
+            return err;
+        };
     }
 
     pub fn row(self: *Conn, sql: []const u8, values: anytype) !?QueryRow {
@@ -305,6 +309,12 @@ pub const Conn = struct {
 
     pub fn rollback(self: *Conn) !void {
         _ = try self.execOpts("rollback", .{}, .{});
+    }
+
+    // Should not be called directly
+    pub fn peekForError(self: *Conn) !void {
+        const data = (try self._reader.peekForError()) orelse return;
+        return self.setErr(data);
     }
 
     // Should not be called directly
@@ -1443,12 +1453,12 @@ test "PG: large read" {
         try t.expectEqual(null, try rows.next());
     }
 
-    {
-        // with a row
-        var row = (try c.row("select $1::text", .{"z" ** 1000})).?;
-        defer row.deinit() catch {};
-        try t.expectString("z" ** 1000, row.get([]u8, 0));
-    }
+    // {
+    //     // with a row
+    //     var row = (try c.row("select $1::text", .{"z" ** 1000})).?;
+    //     defer row.deinit() catch {};
+    //     try t.expectString("z" ** 1000, row.get([]u8, 0));
+    // }
 }
 
 test "Conn: dynamic buffer freed on error" {

src/result.zig
@@ -77,7 +77,7 @@ pub const Result = struct {
 
     pub fn next(self: *Result) !?Row {
         if (self._conn._state != .query) {
-            // Possibly weird state. Most likely cause if calling next() multiple times
+            // Possibly weird state. Most likely cause is calling next() multiple times
             // despite null being returned.
             return null;
         }
@@ -690,6 +690,27 @@ fn getScalar(T: type, data: []const u8, oid: i32) T {
 }
 
 const t = lib.testing;
+test "Result: eager error" {
+    var c = t.connect(.{});
+    defer c.deinit();
+
+    {
+        // Some errors happen when the prepared statement is executed
+        try t.expectError(error.PG, c.query("select * from invalid", .{}));
+        try t.expectString("relation \"invalid\" does not exist", c.err.?.message);
+    }
+
+    {
+        // some errors only happen whemn the result is read
+        try c.begin();
+        defer c.rollback() catch {};
+        const sql = "create temp table test1 (id int) on commit drop";
+        _ = try c.exec(sql, .{});
+        try t.expectError(error.PG, c.query(sql, .{}));
+    }
+
+}
+
 test "Result: ints" {
     var c = t.connect(.{});
     defer c.deinit();

src/reader.zig
@@ -141,11 +141,31 @@ fn ReaderT(comptime T: type) type {
             self.buf = new_buf;
         }
 
-        pub fn next(self: *Self) !Message {
-            return self.buffered(self.pos) orelse self.read();
+        // If you execute "select * from invalid_table", PostgreSQL will return
+        // an error early in the process of preparing the statement - as part
+        // of parsing the statement, it knows that "invalid_table" isn't a valid table.
+
+        // But if you execute "create table already_exists", the error is is only
+        // returned once you try to read the result.
+        //
+        // This difference results in an inconsistent api: some error are returned
+        // immediatelyby conn.query() and some errors are only returned when
+        // result.next() is first called.
+        //
+        // Here we attempt to fix this by eagerly reading the next message. If it's
+        // an error, we return it. If it isn't an error, we put it back for the next
+        // successful read.
+        pub fn peekForError(self: *Self) !?[]const u8 {
+            const message = self.buffered(self.pos, true) orelse try self.read(true);
+            return if (message.type == 'E') message.data else null;
         }
 
-        fn read(self: *Self) !Message {
+
+        pub fn next(self: *Self) !Message {
+            return self.buffered(self.pos, false) orelse self.read(false);
+        }
+
+        fn read(self: *Self, error_peek: bool) !Message {
             const stream = self.stream;
             // const spare = buf.len - pos; // how much space we have left in our buffer
 
@@ -180,7 +200,7 @@ fn ReaderT(comptime T: type) type {
                                 @memcpy(new_buf[0..current_length], buf[start..pos]);
                                 lib.metrics.allocReader(message_length);
                             } else {
-                                // currently using a dynamically alllocated buffer, we'll
+                                // currently using a dynamically allocated buffer, we'll
                                 // grow or allocate a larger one (which is what realloc does)
                                 new_buf = try allocator.realloc(buf, message_length);
                                 if (start > 0) {
@@ -212,14 +232,14 @@ fn ReaderT(comptime T: type) type {
                     return error.Closed;
                 }
                 pos += n;
-                if (self.buffered(pos)) |msg| {
+                if (self.buffered(pos, error_peek)) |msg| {
                     return msg;
                 }
             }
         }
 
         // checks and consume if we already have a message buffered
-        fn buffered(self: *Self, pos: usize) ?Message {
+        fn buffered(self: *Self, pos: usize, error_peek: bool) ?Message {
             const start = self.start;
             const available = pos - start;
 
@@ -240,20 +260,26 @@ fn ReaderT(comptime T: type) type {
             // -4 because the len includes the 4 byte length header itself
             const end = len_end + len - 4;
 
-            // how much extra data we already have
-            const extra = pos - end;
-            if (extra == 0) {
-                // we have no more data in the buffer, reset everything to the start
-                // so that we have the full buffer for future messages
-                self.pos = 0;
-                self.start = 0;
+            const message_type = buf[start];
+
+            if (error_peek == false or message_type == 'E') {
+                // how much extra data we already have
+                const extra = pos - end;
+                if (extra == 0) {
+                    // we have no more data in the buffer, reset everything to the start
+                    // so that we have the full buffer for future messages
+                    self.pos = 0;
+                    self.start = 0;
+                } else {
+                    self.pos = pos;
+                    self.start = end;
+                }
             } else {
                 self.pos = pos;
-                self.start = end;
             }
 
             return .{
-                .type = buf[start],
+                .type = message_type,
                 .data = buf[len_end..end],
             };
         }

src/stmt.zig
@@ -297,11 +297,14 @@ pub const Stmt = struct {
             }
         }
 
+        try conn.peekForError();
+
         // our call to readyForQuery above changed the state, but as far as we're
         // concerned, we're still doing the query.
         conn._state = .query;
 
         lib.metrics.query();
+
         const opts = &self.opts;
         const state = self.result_state;
         const column_count = self.column_count;

commit ae117876d8ea4f0854c5aa5e310d1f566eaa9470

Put connection in fail state if peeking for error fails
Fixes:
https://github.com/karlseguin/pg.zig/issues/44

Issue was introduced in:
adc728df1f19d4ef67d009b80f4cf53aae2bcbd1

src/conn.zig
@@ -204,15 +204,16 @@ pub const Conn = struct {
             return err;
         };
 
-        errdefer stmt.deinit();
-
-        try stmt.prepare(sql);
-
-        inline for (values) |value| {
-            try stmt.bind(value);
+        {
+            errdefer stmt.deinit();
+            try stmt.prepare(sql);
+            inline for (values) |value| {
+                try stmt.bind(value);
+            }
         }
 
         return stmt.execute() catch |err| {
+            stmt.deinit();
             self.maybeRelease(opts.release_conn);
             return err;
         };
@@ -314,6 +315,7 @@ pub const Conn = struct {
     // Should not be called directly
     pub fn peekForError(self: *Conn) !void {
         const data = (try self._reader.peekForError()) orelse return;
+        self._state = .fail;
         return self.setErr(data);
     }
 
@@ -1453,12 +1455,12 @@ test "PG: large read" {
         try t.expectEqual(null, try rows.next());
     }
 
-    // {
-    //     // with a row
-    //     var row = (try c.row("select $1::text", .{"z" ** 1000})).?;
-    //     defer row.deinit() catch {};
-    //     try t.expectString("z" ** 1000, row.get([]u8, 0));
-    // }
+    {
+        // with a row
+        var row = (try c.row("select $1::text", .{"z" ** 1000})).?;
+        defer row.deinit() catch {};
+        try t.expectString("z" ** 1000, row.get([]u8, 0));
+    }
 }
 
 test "Conn: dynamic buffer freed on error" {

src/result.zig
@@ -728,14 +728,36 @@ test "Result: eager error" {
     }
 
     {
-        // some errors only happen whemn the result is read
+        // some errors only happen when the result is read
         try c.begin();
         defer c.rollback() catch {};
         const sql = "create temp table test1 (id int) on commit drop";
         _ = try c.exec(sql, .{});
         try t.expectError(error.PG, c.query(sql, .{}));
     }
+}
 
+// https://github.com/karlseguin/pg.zig/issues/44
+test "Result: eager error conn state" {
+    var pool = try lib.Pool.init(t.allocator, .{ .size = 1, .auth = t.authOpts(.{}) });
+    defer pool.deinit();
+
+    {
+        var c = try pool.acquire();
+        defer c.release();
+
+        // duplicate it
+        _= try c.exec("insert into all_types (id) values ($1)", .{2000});
+        try t.expectError(error.PG, c.exec("insert into all_types (id) values ($1)", .{2000}));
+    }
+
+    {
+        // only 1 connection in our pool, so the fact that athe above fails and
+        // this one suceeds, means we're properly handling the failure
+        var c = try pool.acquire();
+        defer c.release();
+        _= try c.exec("insert into all_types (id) values ($1)", .{2001});
+    }
 }
 
 test "Result: ints" {